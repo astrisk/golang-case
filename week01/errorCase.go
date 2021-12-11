c0cd4f0e00bba0531ba09260e4d08144bf0846e8
ZhangQiuSheng <zhangqiusheng@gridsum.com>
2021-12-11 12:13:32 +0800

    DTO 错误处理示例

---
+++ week01/errorCase.go
@@ -0,0 +1,26 @@
+package week01
+
+import (
+	"database/sql"
+	"errors"
+)
+
+type user struct {
+	Age  int
+	Name string
+	Uid  int64
+}
+
+func getUser(db *sql.DB, uid int64) (*user, error) {
+	user := new(user)
+	sqlStmt := `SELECT account FROM user WHERE uid = ?`
+	rows, err := db.Query(sqlStmt)
+	if err == sql.ErrNoRows {
+		return nil, errors.New("user not found")
+	}
+	if err != nil {
+		return nil, err
+	}
+	err = rows.Scan(&user.Name, &user.Age, &user.Uid)
+	return user, nil
+}
